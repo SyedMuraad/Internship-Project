5f3a6a6eb349bfd7593db7901287a021208b69d9
SyedMuraad <134203145+SyedMuraad@users.noreply.github.com>
2023-08-06 12:17:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,36 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import seaborn as sns
 
 # Load the Mart Sales dataset
 @st.cache
 def load_data():
-    df =  pd.read_csv('Order.tsv',sep='\t',header=0)
-df.head()
+    data = pd.read_csv("mart_sales_dataset.csv")
+    return data
 
 def main():
     st.title("Mart Sales Data Analysis")
     
     # Load the dataset
-    df = load_data()
+    data = load_data()
     
     st.write("## Mart Sales Dataset")
-    st.write(df.head())  # Display the first few rows of the dataset
+    st.write(data.head())  # Display the first few rows of the dataset
     
     # Basic statistics
     st.write("## Basic Statistics")
-    st.write(df.describe())
+    st.write(data.describe())
     
     # Interactive components
     st.write("## Interactive Components")
     
     # Sidebar filters
     st.sidebar.title("Filters")
-    category = st.sidebar.selectbox("Select Category", df['Category'].unique())
+    category = st.sidebar.selectbox("Select Category", data['Category'].unique())
     filtered_data = data[data['Category'] == category]
     
     # Display filtered data
     st.write(f"Displaying data for Category: {category}")
-    st.write(filtered_df)
+    st.write(filtered_data)
     
     # Data visualization
     st.write("## Data Visualization")
